96d40a93ba00c0e86bdbb0c8eabef4b9c0fd3f6a
Ed Doddridge <edoddridge@gmail.com>
2017-05-22 10:05:57 -0400

    open files inside with context managers
    This ensure that the file is closed, regardless of any exceptions that get thrown.

--- verification/tutorial_barotropic_gyre/input/gendata.py
+++ verification/tutorial_barotropic_gyre/input/gendata.py
@@ -24,9 +24,8 @@ def gendata():
   # Walls
   h[-1, :] = 0
   h[:, -1] = 0
-  fid=open('topog.box','wb') 
-  h.astype(ofmt).tofile(fid,"") 
-  fid.close()
+  with open('topog.box','wb') as fid:
+    h.astype(ofmt).tofile(fid,"") 
 
   # Generate zonal wind-stress file that is sin with period of dmain north-south extent
   # and maximum of 0.1 Nm^{2}
@@ -35,9 +34,8 @@ def gendata():
   y = (np.arange(1,ny+1)-0.5) / (ny-1.) # ny-1 accounts for a solid wall
   X, Y = np.meshgrid(x,y)
   tau = tauMax*np.sin(np.pi*Y)
-  fid=open('windx.sin_y','wb') 
-  tau.astype(ofmt).tofile(fid,"") 
-  fid.close()
+  with open('windx.sin_y','wb') as fid:
+    tau.astype(ofmt).tofile(fid,"") 
 
 if __name__ == '__main__':
  gendata()
